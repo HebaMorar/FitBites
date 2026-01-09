16b0b2ce932b80f902b4894b995e9dacdb25b264
heba <hebamorar39@gmail.com>
2026-01-09 19:29:51 +0300

    Update clender.js

--- javascript/clender.js
+++ javascript/clender.js
@@ -127,7 +127,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //emailjs
-        emailjs.send("service_od0jgqx", "template_cdavjpm", {
+        emailjs.send("service_od0jgqx", "template_ve0vtyq", {
             name: userName,
             typeCon: selectedType,
             specialist: chosenDoctor,
@@ -142,6 +142,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     clearButtonStyles();
 });
+
 
 
 
